760e26d192bc65ab2cb8e416374090c7435abf18
APLS <html6@msn.com>
2020-03-15 23:44:56 +0800

    删除 quartz ，使用rabbitMq

--- src/main/java/io/qyi/e5/config/redis/RedisConfig.java
+++ src/main/java/io/qyi/e5/config/redis/RedisConfig.java
@@ -3,12 +3,9 @@ package io.qyi.e5.config.redis;
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.PropertyAccessor;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.springframework.boot.autoconfigure.AutoConfigureAfter;
-import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.CachingConfigurerSupport;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.cache.interceptor.KeyGenerator;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheManager;
@@ -17,41 +14,34 @@ import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
-import java.lang.reflect.Method;
-
 /**
  * @author JiaweiWu
  * @description: redis配置
  */
 @Configuration
 //@AutoConfigureAfter(RedisAutoConfiguration.class)
-//@EnableCaching
+@EnableCaching
 public class RedisConfig extends CachingConfigurerSupport {
 
-    /**
-     * 自定义生成key的规则
-     */
-   /* @Override
-    public KeyGenerator keyGenerator() {
-        return new KeyGenerator() {
-            @Override
-            public Object generate(Object o, Method method, Object... objects) {
-                //格式化缓存key字符串
-                StringBuilder sb = new StringBuilder();
-                //追加类名
-                sb.append(o.getClass().getName());
-                //追加方法名
-                sb.append(method.getName());
-                //遍历参数并且追加
-                for (Object obj : objects) {
-                    sb.append(obj.toString());
-                }
-                System.out.println("调用Redis缓存Key : " + sb.toString());
-                return sb.toString();
-            }
-        };
-    }
-*/
+
+    /*@Bean
+    RedisMessageListenerContainer container(RedisConnectionFactory connectionFactory
+                                            ,MessageListenerAdapter listenerAdapter
+                                            ,MessageListenerAdapter catAdapter) {
+        RedisMessageListenerContainer container = new RedisMessageListenerContainer();
+        container.setConnectionFactory(connectionFactory);
+        //订阅了一个叫chat 的通道
+        container.addMessageListener(listenerAdapter, new PatternTopic("__keyevent@1__:expired"));
+
+        container.addMessageListener(catAdapter, new PatternTopic("__keyevent@1__:expired"));
+        return container;
+    }*/
+
+   /* @Bean
+    MessageListenerAdapter catAdapter() {
+        return new MessageListenerAdapter(new FishListener());
+    }*/
+
     /**
      * 采用RedisCacheManager作为缓存管理器
      *
@@ -64,34 +54,6 @@ public class RedisConfig extends CachingConfigurerSupport {
         //rcm.setDefaultExpiration(60);//秒
         return redisCacheManager;
     }
-
-    /**
-     * 配置自定义redisTemplate
-     * @return
-     */
-    /*@Bean
-    RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
-
-        RedisTemplate<String, Object> template = new RedisTemplate<>();
-        template.setConnectionFactory(redisConnectionFactory);
-
-        //使用Jackson2JsonRedisSerializer来序列化和反序列化redis的value值
-        Jackson2JsonRedisSerializer serializer = new Jackson2JsonRedisSerializer(Object.class);
-
-        ObjectMapper mapper = new ObjectMapper();
-        mapper.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
-        mapper.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
-        serializer.setObjectMapper(mapper);
-
-        template.setValueSerializer(serializer);
-        //使用StringRedisSerializer来序列化和反序列化redis的key值
-        template.setKeySerializer(new StringRedisSerializer());
-        template.setHashKeySerializer(new StringRedisSerializer());
-        template.setHashValueSerializer(serializer);
-        template.afterPropertiesSet();
-        return template;
-    }*/
-
 
     @Bean
     @SuppressWarnings("all")
@@ -115,6 +77,5 @@ public class RedisConfig extends CachingConfigurerSupport {
         template.afterPropertiesSet();
         return template;
     }
-
 
 }
